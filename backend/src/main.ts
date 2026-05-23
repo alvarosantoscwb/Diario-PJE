2d8fc4202d8284bd400ca92a1bb449c0f7a7b36f
alvarosantoscwb <alvarojuniorctb@hotmail.com>
2026-05-23 15:38:43 -0300

    fix: adjust main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.BACKEND_PORT ?? 3001);
+  await app.listen(process.env.PORT ?? process.env.BACKEND_PORT ?? 3001);
 }
 bootstrap();
